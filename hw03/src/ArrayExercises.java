8940b51d3047cf583c1d7f41c7c0564827d7642b
Matthew Clark <mattwclark@gmail.com>
2025-09-12 20:01:20 -0700

    prepping for upstream changes

--- hw03/src/ArrayExercises.java
+++ hw03/src/ArrayExercises.java
@@ -2,13 +2,27 @@ public class ArrayExercises {
     /** Returns the second to last item in the given array.
      *  Assumes the array has at least 2 elements. */
     public static String secondToLastItem(String[] items) {
-        // TODO: Implement this method
-        return null;
+        int length = 0;
+        for (String i : items) {
+            length++;
+        }
+        //Minus one for length to index conversion and -1 for second to last
+        return items[length-1-1];
+
     }    
 
     /** Returns the difference between the minimum and maximum item in the given array */
     public static int minMaxDifference(int[] items) {
-        // TODO: Implement this method
-        return 0;           
+        int min = items[0];
+        int max = items[0];
+        for (int i : items){
+            if(i<min){
+                min = i;
+            }
+            if(i>max){
+                max = i;
+            }
+        }
+        return max-min;
     }
 }
