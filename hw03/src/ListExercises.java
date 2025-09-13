9d1584a048307aa41170cf79cc5870d431eef8e6
Matthew Clark <mattwclark@gmail.com>
2025-09-12 20:01:20 -0700

    prepping for upstream changes

--- hw03/src/ListExercises.java
+++ hw03/src/ListExercises.java
@@ -1,33 +1,63 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ListExercises {
     /** Returns the total sum in a list of integers */
     public static int sum(List<Integer> L) {
-        // TODO: Implement this method
-        return 0;
+        int sum_int = 0;
+
+        for (int elem : L){
+            sum_int = sum_int + elem;
+        }
+
+        return sum_int;
     }
 
     /** Returns a list containing the even numbers of the given list */
     public static List<Integer> evens(List<Integer> L) {
-        // TODO: Implement this method
-        return null;
+        List<Integer> even_nums = new ArrayList<>();
+        for (int elem : L){
+            if (elem % 2 == 0){
+                even_nums.add(elem);
+            }
+
+        }
+        return even_nums;
     }
 
     /** Returns a list containing the common item of the two given lists */
     public static List<Integer> common(List<Integer> L1, List<Integer> L2) {
-        // TODO: Implement this method
-        return null;
+        List<Integer> common_items = new ArrayList<>();
+        for (int elem : L1){
+            if (L2.contains(elem)){
+                common_items.add(elem);
+            }
+        }
+        return common_items;
     }
 
     public static int countOccurrencesOfWord(List<String> words, String w) {
-        // TODO: Implement this method
-        return 0;
+        int occurances = 0;
+        for (String word : words){
+            if (Objects.equals(word, w)){
+                occurances++;
+            }
+        }
+
+        return occurances;
     }
 
     /** Returns the number of occurrences of the given character in a list of strings. */
     public static int countOccurrencesOfC(List<String> words, char c) {
-        // TODO: Implement this method
-        return 0;
+        int occurances = 0;
+        for (String word : words){
+            for (int i = 0; i<word.length(); i++){
+                if (word.charAt(i) == c){
+                    occurances ++;
+                }
+            }
+        }
+        return occurances;
     }
 }
--- hw03/src/MapExercises.java
+++ hw03/src/MapExercises.java
@@ -1,27 +1,44 @@
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-
+import java.util.HashMap;
 public class MapExercises {
     /** Returns a map from every lower case letter to the number corresponding to that letter, where 'a' is
      * 1, 'b' is 2, 'c' is 3, ..., 'z' is 26.
      */
     public static Map<Character, Integer> letterToNum() {
-        // TODO: Implement this method
-        return null;
+        Map<Character, Integer> chars_to_nums = new HashMap<>();
+        for (char letter = 'a'; letter <= 'z'; letter++)
+        {
+            int number = letter - 'a' + 1;
+            chars_to_nums.put(letter, number);
+        }
+        return chars_to_nums;
     }
 
     /** Returns a map from the integers in the list to their squares. For example, if the input list
      *  is [1, 3, 6, 7], the returned map goes from 1 to 1, 3 to 9, 6 to 36, and 7 to 49.
      */
     public static Map<Integer, Integer> squares(List<Integer> nums) {
-        // TODO: Implement this method
-        return null;
+        Map<Integer, Integer> ints_to_squares = new HashMap<>();
+        for (int a : nums){
+            ints_to_squares.put(a, a * a);
+        }
+        return ints_to_squares;
     }
 
     /** Returns a map of the counts of all words that appear in a list of words. */
     public static Map<String, Integer> countWords(List<String> words) {
-        // TODO: Implement this method
-        return null;
+        Map<String, Integer> list_occurrences = new HashMap<>();
+        for (String word : words){
+            if (list_occurrences.containsKey(word)){
+                int value = list_occurrences.get(word);
+                list_occurrences.put(word, value + 1);
+            }
+            else{
+                list_occurrences.put(word, 1);
+            }
+        }
+        return list_occurrences;
     }
 }
